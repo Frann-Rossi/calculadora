e31e9ee43656e9feab1efd8527d5c9e58c764dbd
Franco Matias Rossi <95671766+frannqito@users.noreply.github.com>
2023-04-19 17:30:32 -0300

    first commit

---
+++ js/Calculadora.js
@@ -0,0 +1,17 @@
+class Calculadora {
+	sumar(num1 = 0, num2 = 0) {
+		return num1 + num2;
+	}
+
+	restar(num1 = 0, num2 = 0) {
+		return num1 - num2;
+	}
+
+	multiplicar(num1 = 0, num2 = 0) {
+		return num1 * num2;
+	}
+
+	dividir(num1 = 0, num2 = 0) {
+		return num1 / num2;
+	}
+}
